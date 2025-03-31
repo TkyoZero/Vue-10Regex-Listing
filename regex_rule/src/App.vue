812db220135c4a34df1dafe312bcda9a014e1a17
T.T <tenzin.tob8@gmail.com>
2025-03-31 16:39:02 +0200

    started header & footer & costum theme

--- regex_rule/src/App.vue
+++ regex_rule/src/App.vue
@@ -3,6 +3,14 @@ import { RouterLink, RouterView } from 'vue-router'
 </script>
 
 <template>
+  <header>
+    <h1 class="m-4 p-4 text-center text-5xl font-display">Regex Rules</h1>
+  </header>
+
+  <footer class="mt-20 p-20 bg-green-950 text-white-200">
+    <div>Souces:</div>
+    <div class="text-center">&copy Tenzin Tobatsang</div>
+  </footer>
   <RouterView />
 </template>
 
