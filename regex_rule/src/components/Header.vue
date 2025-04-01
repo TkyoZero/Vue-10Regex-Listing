894cabf4d979d798eb4c11114481e01aa4f007c1
T.T <tenzin.tob8@gmail.com>
2025-04-01 13:05:07 +0200

    create component header

---
+++ regex_rule/src/components/Header.vue
@@ -0,0 +1,7 @@
+<template>
+    <header class="mt-20 p-20 bg-green-950 text-white-200">
+    <RouterLink to="/">
+      <h1 class="m-4 p-4 text-center text-5xl font-display">Regex Rules</h1>
+    </RouterLink>
+  </header>
+</template>
