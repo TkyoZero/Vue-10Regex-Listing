c02a216827231b45292e6d24caed1f9511aca876
T.T <tenzin.tob8@gmail.com>
2025-04-01 13:04:58 +0200

    create component footer

---
+++ regex_rule/src/components/Footer.vue
@@ -0,0 +1,12 @@
+<template>
+    <footer class="mt-20 p-20 bg-green-950 text-white-200">
+    <div class="text-xl font-medium">Credits:</div>
+    <div class="text-emerald-500">
+      <div>
+        <a href="https://regexlearn.com/">RegexLearn</a>
+      </div>
+      <div><a href="https://developer.mozilla.org/en-US/docs/Web/JavaScript/Guide/Regular_expressions">MDN</a></div>
+    </div>
+    <div class="text-center">&copy Tenzin Tobatsang</div>
+  </footer>
+</template>
